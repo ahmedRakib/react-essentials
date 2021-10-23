407ad1032d3fb8ccffb703f478120624eec216c7
Mohammad Rakibul Hasan <rkhasan333@gmail.com>
2021-10-23 22:13:55 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Arena</h1>
+    </header>
+  )
+}
+
+function Main() {
+  return(
+    <section>
+      <p>Welcome to my world. Let the game begin</p>
+    </section>
+  )
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright @ Rakib {props.date}</p>
+    </footer>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Rakib" />
+      <Main />
+      <Footer date={new Date().getFullYear()} />
     </div>
   );
 }
